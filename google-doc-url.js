c2e26ab213d918c0b60c82ecef636cc2fb55a421
eiseledyrli <55553644+eiseledyrli@users.noreply.github.com>
2019-09-19 13:29:57 -0400

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1_d1tCcrNuiZioPH4hDzpuOXcw1PUMs__FNwV6YZW9S0/edit#gid=0';
